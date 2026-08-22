08efbf4c9495601f24b67fd8abbeb3e96ef9d5be
Geordie - <Geordietomomcclelland@gmail.com>
2026-08-22 14:30:52 +1000

    re-add texture include to style_box_flat

--- scene/resources/style_box_flat.cpp
+++ scene/resources/style_box_flat.cpp
@@ -33,6 +33,7 @@
 #include "core/config/engine.h"
 #include "core/object/callable_mp.h"
 #include "core/object/class_db.h"
+#include "scene/resources/texture.h"
 #include "servers/rendering/rendering_server.h"
 #include "servers/text/text_server.h"
 
